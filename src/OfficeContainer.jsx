2f518080233a12885b88111dd4ac58c5da9bc86c
kofesto <kofesto@hotmail.com>
2022-03-30 17:26:55 +0100

    Added state data to country

--- src/OfficeContainer.jsx
+++ src/OfficeContainer.jsx
@@ -2,29 +2,37 @@ import React,{useState} from 'react';
 import offices from './office.json';
 
 function OfficeContainer() {
-  const [nigeria,setNigeria] = useState('');
-  const [usa,setUsa] = useState('');
-  const [kenya,setKenya] = useState('');
+  const [city,setCity] = useState('');
   //console.log(offices)
   const ng = (country)=>{
-    offices.filter(office => office.country === country).map(ng=>{
-       return setNigeria(ng);
+    offices.filter(office => office.country === country).map(data=>{
+       return setCity(data.city);
     })
   }
   
-  console.log(nigeria)
+  console.log(city)
   return (
   <>
   <div>
     <div className="country">
       <span>
         <h2>Nigeria</h2>
+        <p>{city ? city.map(c=> { return c}) : ""}</p>
       </span>
       <button className="show-city" onClick={()=>ng('Nigeria')}>Show</button>
     </div>
-    <div className="country">USA
-    <button className="show-city" onClick={()=>ng('USA')}>show</button></div>
-    <div className="country">Kenya<button className="show-city">show</button></div>
+    <div className="country">
+      <span>
+        <h2>USA</h2>
+      </span>
+      <button className="show-city" onClick={()=>ng('USA')}>show</button>
+    </div>
+      <div className="country">
+        <span>
+          <h2>Kenya</h2>
+        </span>
+        <button className="show-city" onClick={()=>ng('Kenya')}>show</button>
+      </div>
   </div>
   </>
   );
